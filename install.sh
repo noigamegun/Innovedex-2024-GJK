9a3ea7a12539aca5e5b87ea7e371454ca0da9b7c
Game <thapat.g11@gmail.com>
2024-08-07 20:24:08 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,63 @@
 #!/bin/bash
 
+# Exit IF a command fail
+
+set -e
+
+# Intro
+echo "Game's ROS Noetic Installer"
+echo "============================================="
+echo "This is a script to quickly install ROS Noetic."
+echo "The commands that will be run is from the ROS Noetic installation guide for Ubuntu Focal. It may not work with older or newer version. Support for Focal ends in April 2025."
+echo "This script should be RUN AS THE USER NOT ROOT."
+echo "(Copyright) Thapat A. GPLv3"
+
+# Wait for user to read intro
+echo "Waiting 30 seconds to start. Ctrl+C to abort."
+sleep 30
+
+# Wait for user to configure repos.
+echo "Configure your Ubuntu repositories to allow restricted, universe and multiverse repo. Waiting 10 seconds... Ctrl+C to abort."
+sleep 10
+
 # Request SU Perms. Abort if ran as root.
+if [ "$EUID" -ne 0 ]
+  then echo "Root detected. Aborting..."
+  exit
+fi
+
+echo "Requesting Superuser... Enter your password if prompted."
 sudo
 
+# Update apt
+echo "Updating apt repos..."
+sudo apt update
+
+# Add ROS Noetic repo to sources.list.d/
+echo "Adding ROS Noetic repos..."
+sudo echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list
+
+# Add keys
+echo "Installing curl..."
+sudo apt install curl
+
+echo "Adding signing keys..."
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+
+# Update repos
+echo "Updating repos..."
+sudo apt update
+
+# Install ROS
+echo "Installing ROS..."
+sudo apt install ros-noetic-desktop-full
+
+# Add ROS to .bashrc
+echo "Adding ROS to .bashrc..."
+echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
+
+# Add extra depends
+echo "Installing extra dependencies..."
+sudo apt install python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential
+
+echo "Install done! Restart bash to take effect."
