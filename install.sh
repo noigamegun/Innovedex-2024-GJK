f9f5041d4ddae744b07e44be74b90a7077b8a4d9
Game <thapat.g11@gmail.com>
2024-08-07 20:56:26 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ echo "Game's ROS Noetic Installer"
 echo "============================================="
 echo "This is a script to quickly install ROS Noetic."
 echo "The commands that will be run is from the ROS Noetic installation guide for Ubuntu Focal. It may not work with older or newer version. Support for Focal ends in April 2025."
-echo "This script should be RUN AS THE USER NOT ROOT."
+echo "This script should be RUN AS ROOT."
 echo "(Copyright) Thapat A. GPLv3"
 
 # Wait for user to read intro
@@ -22,26 +22,26 @@ sleep 10
 
 # Update apt
 echo "Updating apt repos..."
-sudo apt update
+apt update
 
 # Add ROS Noetic repo to sources.list.d/
 echo "Adding ROS Noetic repos..."
-sudo echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list
+echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list
 
 # Add keys
 echo "Installing curl..."
-sudo apt install curl
+apt install curl
 
 echo "Adding signing keys..."
-curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | apt-key add -
 
 # Update repos
 echo "Updating repos..."
-sudo apt update
+apt update
 
 # Install ROS
 echo "Installing ROS..."
-sudo apt install ros-noetic-desktop-full
+apt install ros-noetic-desktop-full
 
 # Add ROS to .bashrc
 echo "Adding ROS to .bashrc..."
@@ -49,6 +49,6 @@ echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
 
 # Add extra depends
 echo "Installing extra dependencies..."
-sudo apt install python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential
+apt install python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential
 
 echo "Install done! Restart bash to take effect."
