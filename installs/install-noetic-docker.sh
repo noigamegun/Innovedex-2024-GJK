df3f6796fa282810e28c31841afa78e9bd81232a
noigamegun <76461912+noigamegun@users.noreply.github.com>
2024-08-13 13:50:00 +0700

    Compressed Lines

--- installs/install-noetic-docker.sh
+++ installs/install-noetic-docker.sh
@@ -31,10 +31,7 @@ chmod a+r /etc/apt/keyrings/docker.asc
 echo "Adding repos..."
 
 # Add the repository to Apt sources:
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  tee /etc/apt/sources.list.d/docker.list > /dev/null
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu$(. /etc/os-release && echo "$VERSION_CODENAME") stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 # Update Repo
 echo "Updating apt repos...
